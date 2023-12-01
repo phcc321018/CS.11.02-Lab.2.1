bac9f59b922a9c420227db1d2fa6d4a510db48da
phcc321018 <phcc32@student.cis.edu.hk>
2023-12-01 12:46:51 +0800

    lab 2.1

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,48 @@ public class Main {
     }
 
     // 1. add
+    public static int add(int a, int b) {
+        return a + b;
+    }
     
     // 2. add
+    public static int add(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
 
     // 3. morningGreeting
+    public static String morningGreeting(String name) {
+        return "早上好, " + name + "!";
+    }
 
     // 4. afternoonGreeting
-
+    public static String afternoonGreeting(String name)
+    {
+        return name;
+    }
     // 5. triple
-
+    public static String triple(String word)
+    {
+        return word;
+    }
+        public static String triple(String word) {
+            return word + word + word;
+        }
     // 6. half
-
+    public static double half(int number) {
+        return number / 2.0;
+    }
     // 7. roundPositiveValueToNearestInteger
+    public static int roundPositiveValueToNearestInteger(double number) {
+        int roundedValue = (int) Math.round(number);
+        return roundedValue;
+    }
+
 
     // 8. roundNegativeValueToNearestInteger
+    public static int roundNegativeValueToNearestInteger(double number) {
+        int roundedValue = (int) Math.round(number);
+        return roundedValue;
 
-}
+    }
+
